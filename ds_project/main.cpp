f0b1054da86fb5c81d6b24523bacbc73a00ed0c6
Nishanth-Gobi <55394089+Nishanth-Gobi@users.noreply.github.com>
2020-03-24 14:00:25 +0530

    Update main.cpp
    1) New functions :
                  -> is_last_node (To check if a given node is the last letter of any word in the dictionary)
                  -> get_ele (Returns the most appropriate word in the dictionary as a replacement for he string in the input text file)
    2) The input is a text file named "text.txt"
    3)The corrected text is written in a file named "out.txt"

--- ds_project/main.cpp
+++ ds_project/main.cpp
@@ -2,14 +2,17 @@
 #include <stdio.h>
 #include<string.h>
 #include<windows.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 #define nullptr 0
+//#define CHAR_TO_POSITION(a) ((int)a - (int))  Shouldn't this be used ??
+
 class trie{
     public:
                 bool endx;
-                trie *chr[256];
+                trie *chr[256]; // Why 256???
                 trie()
                 {
                     for(int i=0;i<256;i++)
@@ -20,6 +23,7 @@ class trie{
                 }
                 void insert_ele(char ch[],int n);
                 int search_ele(char ch[],int n);
+                char* get_ele(char ch[],int n);
 };
 void trie::insert_ele(char ch[],int n)
 {
@@ -51,6 +55,104 @@ int trie::search_ele(char ch[],int n)
     return int(t->endx);
 }
 
+bool is_last_node(trie *t)
+{
+    return t->endx;
+}
+
+char* trie::get_ele(char ch[], int n)
+{
+    cout<<"In get_ele : "<<ch<<endl;
+    trie *t = this;
+    int i, cnt=1;
+    static char ret[50];
+    for(i=0;i<n;i++)
+    {
+        if(t->chr[ch[i]]!=nullptr)
+        {
+            cout<<"In trie : "<<ch[i]<<endl;
+            t = t->chr[ch[i]];
+            cnt++;
+        }
+        else
+        {
+            cout<<"Not in trie : "<<ch[i]<<endl;
+            if(is_last_node(t))
+            {
+                cout<<"In last node"<<endl;
+                int k;
+                for(k=0;k<cnt-1;k++)
+                {
+                    ret[k] = ch[k];
+                }
+                ret[k] = '\0';
+                cout<<"Ret : "<<ret<<endl;
+                if(!search_ele(ret,strlen(ret)))
+                {
+                    cout<<"Not in Dic!!!"<<endl;
+                }
+                return ret;
+            }
+            else
+            {
+                cout<<"Not in last node "<<endl;
+                int k;
+                for(k=0;k<cnt-1;k++)
+                {
+                    ret[k] = ch[k];
+                }
+                while(!is_last_node(t))
+                {
+                    char j = 'a';
+                    while(j<='z')
+                    {
+                        if(t->chr[j]!=nullptr)
+                        {
+                            ret[k] = j;
+                            k++;
+                            t = t->chr[j];
+                        }
+                        else
+                        {
+                            j = (int)j +1;
+                        }
+                    }
+                }
+                ret[k] = '\0';
+                cout<<"ret : "<<ret<<endl;
+                return ret;
+            }
+        }
+
+    }
+    if(cnt==n+1) // If the given string isn't a word but a part of an actual word
+    {
+        cout<<"Sub-string : "<<ch<<endl;
+        int k = n;
+        strcpy(ret,ch);
+        while(! is_last_node(t))
+        {
+            char j='a';
+            while(j<='z')
+            {
+                if(t->chr[j]!=nullptr)
+                {
+                    ret[k] = j;
+                    k++;
+                    t = t->chr[j];
+                    break;
+                }
+                else
+                {
+                    j = (int)j + 1;
+                }
+            }
+        }
+        ret[k] = '\0';
+        cout<<"Ret in prefix : "<<ret<<endl;
+        return ret;
+    }
+}
 
 struct words {
                 char x[50];
@@ -58,26 +160,62 @@ struct words {
 
 int main()
 {
-    char x[50];
     words w;
-    FILE *q;
-
     trie t;
     char ch[50];
-    cout<<"loading"<<endl;
-    q=fopen("words2.txt","r+");
-
-    while(!feof(q))
+    cout<<"Loading..."<<endl;
+    fstream q;
+    string input_file;
+    input_file = "english3.txt";
+    q.open(input_file.c_str());
+    unsigned int j;
+    while(q>>ch)
     {
-        fread(&w,sizeof(words),1,q);
-        strcpy(ch,w.x);
-        ch[strlen(ch)-1]='\0';
+        for(j=0;j<strlen(ch);j++)
+        {
+            ch[j] = tolower(ch[j]);
+        }
+        ch[0] -= 32;
+        ch[strlen(ch)]='\0';
         t.insert_ele(ch,strlen(ch));
     }
     system("cls");
-    cout<<"enter element to be searched :"<<endl;
-    fflush(stdin);
-    gets(ch);
-    cout<<bool(t.search_ele(ch,strlen(ch)))<<endl;
+    cout<<"Enter a word : ";
+    cin>>ch;
+    for(j=0;j<strlen(ch);j++)
+    {
+        ch[j] = tolower(ch[j]);
+    }
+    ch[strlen(ch)]='\0';
+    ch[0] -= 32;
+    cout<<t.search_ele(ch,strlen(ch))<<endl;
+    fstream p;
+    string filename;
+    filename = "text.txt";
+    p.open(filename.c_str());
+    ofstream outfile;
+    outfile.open("out.txt");
+    while(p>>ch)
+    {
+        for(j=0;j<strlen(ch);j++)
+        {
+            ch[j] = tolower(ch[j]);
+        }
+        ch[strlen(ch)]='\0';
+        ch[0] -= 32;
+        cout<<"input : "<<ch<<endl;
+        if(!bool(t.search_ele(ch,strlen(ch))))
+        {
+            cout<<"Not_in_dic : "<<ch<<endl;
+            char *st = t.get_ele(ch,strlen(ch));
+            cout<<"Output : "<<st<<endl;
+            strcpy(ch,st);
+        }
+        ch[0] +=32;
+        outfile<<ch<<" ";
+    }
+    q.close();
+    p.close();
+    outfile.close();
     return 0;
 }
